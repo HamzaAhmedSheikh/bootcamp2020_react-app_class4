ad477e991b84512e40b70df8a2aabcb185d0de6d
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-13 16:29:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {
 
  let [count, setCount] = useState(1)
-//  let [isTheme, setTheme] = useState(false)
+ let [isTheme, setTheme] = useState(false)
 
   return (
     <div className="App">
@@ -21,6 +21,10 @@ function App() {
        <button onClick={() => setCount(--count)}className="btn btn-outline-warning btn-sm m-2"> Decrement Counter </button>
        <button onClick={() => setCount(0)} className="btn btn-outline-danger btn-sm m-2"> Reset </button>
        
+       <div className={`container-fluid bg p-5 mt-4 mb-4 p-3 head ${isTheme ? 'headOne': '' }`}>
+        <h1 className="text-center"> Bootcamp 2020 Assignment #4 </h1>
+        <button onClick={() => setTheme(!isTheme)} className="btn btn-outline-success btn-sm m-2"> Update Theme </button>
+      </div>
        
     </div>
   );

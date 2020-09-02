529506fe29fe444085fb48c3305d51071db46e9d
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-02 05:23:16 +0500

    error in fcm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Counter from './Counter'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
+import { initNotification } from './Services/FirebaseService'
+
 function App() {
 
  let [count, setCount] = useState(1)
@@ -20,6 +22,7 @@ function App() {
        <button onClick={() => setCount(++count)} className="btn btn-outline-success btn-sm m-2"> Update Counter </button>
        <button onClick={() => setCount(--count)}className="btn btn-outline-warning btn-sm m-2"> Decrement Counter </button>
        <button onClick={() => setCount(0)} className="btn btn-outline-danger btn-sm m-2"> Reset </button>
+       <button onClick={initNotification} className="btn btn-outline-success btn-sm m-2"> Get Notification </button>
        
        <div className={`container-fluid bg p-5 mt-4 mb-4 p-3 head ${isTheme ? 'headOne': '' }`}>
         <h1 className="text-center"> Bootcamp 2020 Assignment #4 is done.  </h1>
